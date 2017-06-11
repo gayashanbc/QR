563d6e96365ca1be81df8311cd6846f46e5db09b
Gayashan Bombuwala <gayashanbc@gmail.com>
2017-06-11 15:49:21 +0530

    Enable QR code reading

---
+++ Assets/Scripts/Reader.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using ZXing;
+
+public class Reader : MonoBehaviour
+{
+    public Texture2D inputTexture; 
+
+    void Start()
+    {
+        // create a barcode reader instance
+        IBarcodeReader reader = new BarcodeReader();
+        // get texture Color32 array
+        var barcodeBitmap = inputTexture.GetPixels32();
+        // detect and decode the barcode inside the Color32 array
+        var result = reader.Decode(barcodeBitmap, inputTexture.width, inputTexture.height);
+        // do something with the result
+        if (result != null)
+        {
+            Debug.Log(result.BarcodeFormat.ToString());
+            Debug.Log(result.Text);
+
+            if (result.Text.Equals("http://gayashan.net"))
+            {
+                Debug.Log("Wade goda oii!!");
+            }
+            else
+            {
+                Debug.Log("hari giye na");
+            }
+        }
+    }
+}
